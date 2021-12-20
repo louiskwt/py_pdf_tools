b6a89aae7dbb416da0d2a7f826b2774a1b9a09fb
Ka Wing Tsang <kawingt2@gmail.com>
2021-12-20 21:51:32 +0800

    added comment

--- img_ocr.py
+++ img_ocr.py
@@ -1,17 +1,19 @@
 from PIL import Image
 import pytesseract
 
-# Set up output
+# Set up output file
 output = 'output.txt'
 f = open(output, 'a')
 
 # Load the img
 filename = 'test.jpeg'
 
+# extract the text from image
 text = str(((pytesseract.image_to_string(Image.open(filename)))))
 
 text = text.replace('-\n', '')
 
+# write to output file
 f.write(text)
 
 f.close()
